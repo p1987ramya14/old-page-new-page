6910f7cd0948456bbcd9d89aa7067d7d2999b9bf
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-08-14 17:23:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 error_reporting(0);
 
 // The new URL to redirect to
-$new_url = 'https://www.amazon.com/Simple-Joys-Carters-Short-Sleeve-Bodysuit/dp/B07GY1RRZF';
+$new_url = 'https://www.amazon.com/stores/luxury/page/EB939B9B-AF9F-49CB-9C14-20B848A813BF';
 
 // Send the 301 Moved Permanently header
 header("Location: $new_url", true, 301);
